328e1a8511d177d63aa7b791b901bfbd55f1af36
Alex Kimeu <alexkimeu1999@gmail.com>
2024-02-14 20:04:38 +0300

    Api Deployment

--- server.js
+++ server.js
@@ -8,12 +8,10 @@ const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 // Add this before server.use(router)
-server.use(
- // Add custom route here if needed
- jsonServer.rewriter({
-  "/api/*": "/$1",
- })
-);
+server.use(jsonServer.rewriter({
+    '/api/*': '/$1',
+    '/blog/:resource/:id/show': '/:resource/:id'
+}))
 server.use(router);
 // Listen to port
 server.listen(3000, () => {
